a406e2ac42b64223cfaae1a76daa927cc70a40ce
Juga Paazmaya <paazmaya@yahoo.com>
2018-10-30 17:06:51 +0200

    Configuration for unit tests #1, code coverage #2, and linting

--- index.js
+++ index.js
@@ -15,114 +15,13 @@
 const fs = require('fs'),
   path = require('path');
 
-const got = require('got');
-
-const GITHUB_API = 'https://api.github.com';
-
-/**
- * Create an issue to the given repository.
- *
- * @param {string} repo person/name
- * @return {Promise}
- * @see https://developer.github.com/v3/issues/#create-an-issue
- */
-const createIssue = (repo, options) => {
-  const url = `${GITHUB_API}/repos/${repo}/issues`;
-  const data = {
-    title: 'Limit the included files in the package published to npm',
-    body: `Seems that neither 'files' property is used in 'package.json' to whitelist the included files, nor '.npmignore' file is being used for blacklisting included files.
-    `
-  };
-
-  return got(url, {
-    json: true,
-    headers: {
-      'accept': 'application/vnd.github.v3+json',
-      'authorization': `token ${options.token}`,
-      'user-agent': 'https://github.com/paazmaya/tawata'
-    },
-    method: 'POST',
-    body: data
-  }).then(res => {
-    console.log('url', url);
-    console.log(res.body);
-  }).catch(error => {
-    console.log(error.response);
-    //=> 'Internal server error ...'
-  });
-};
-
-/**
- * Get the .npmignore file from the repository.
- *
- * @param {string} repo person/name
- * @return {Promise}
- * @see https://developer.github.com/v3/repos/contents/#get-contents
- */
-const getRepoIgnore = (repo, options) => {
-  const url = `${GITHUB_API}/repos/${repo}/contents/.npmignore`;
-
-  return got(url, {
-    json: true,
-    headers: {
-      'accept': 'application/vnd.github.v3+json',
-      'authorization': `token ${options.token}`,
-      'user-agent': 'https://github.com/paazmaya/tawata'
-    },
-    method: 'GET'
-  }).then(response => {
-    console.log('url', url);
-    console.log(response.statusCode);
-    console.log(Objec.keys(response));
-  }).catch(error => {
-    console.log(error.response.statusCode);
-    console.log(Objec.keys(error.response));
-    console.log(error.response);
-    //=> 'Internal server error ...'
-  });
-};
-
 const parseContent = (body) => {
   if (body.content && body.encoding) {
-    // https://nodejs.org/api/buffer.html#buffer_class_method_buffer_from_string_encoding
-    // added in Node.js 5.10
     const data = Buffer.from(body.content, body.encoding);
+
     return data.toString('utf8');
   }
   return body;
-};
-
-/**
- * Get the package.json file from the repository.
- *
- * @param {string} repo person/name
- * @return {Promise}
- * @see https://developer.github.com/v3/repos/contents/#get-contents
- */
-const getRepoPackage = (repo, options) => {
-  const url = `${GITHUB_API}/repos/${repo}/contents/package.json`;
-
-  return got(url, {
-    json: true,
-    headers: {
-      'accept': 'application/vnd.github.v3+json',
-      'authorization': `token ${options.token}`,
-      'user-agent': 'https://github.com/paazmaya/tawata'
-    },
-    method: 'GET'
-  }).then(response => {
-    console.log('url', url);
-    console.log(response.body);
-    console.log(response.statusCode);
-    return response.body;
-  }).then(parseContent).then(data => {
-    return parseJson(data);
-  }).then(pkg => {
-    return checkFilesProperty(pkg, options);
-  }).catch(error => {
-    console.log(error.response);
-    //=> 'Internal server error ...'
-  });
 };
 
 /**
---
+++ tests/lib/parse-content_test.js
@@ -0,0 +1,30 @@
+
+const tape = require('tape'),
+  parseContent = require('../../lib/parse-content');
+
+tape('parseContent - returns self if no encoding', (test) => {
+  test.plan(1);
+
+  const input = {
+    stuff: ''
+  };
+
+  const output = parseContent(input);
+
+  test.deepEqual(output, input);
+});
+
+tape('parseContent - reads input as expected', (test) => {
+  test.plan(1);
+
+  const text = 'senkin tonttu';
+
+  const input = {
+    content: text,
+    encoding: 'utf8'
+  };
+
+  const output = parseContent(input);
+
+  test.equal(output, text);
+});
